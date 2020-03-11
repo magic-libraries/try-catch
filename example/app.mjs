600b75b46fe800aec0e0cb39edfd113874d9b9a1
jascha <jascha@jaeh.at>
2020-03-11 07:07:52 +0100

    update title

--- example/app.mjs
+++ example/app.mjs
@@ -2,7 +2,7 @@ export const state = {
   title: '@magic-libraries/try-catch',
   description: 'try-catch client lib for @magic. returns errors instead of throwing them.',
 
-  logotext: '@magic-libraries/try-catch',
+  logotext: 'try-catch',
   menu: [
     { to: '/#installation', text: 'installation' },
     { to: '/#usage', text: 'usage' },
